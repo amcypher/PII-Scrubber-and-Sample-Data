dee3f32d8b0b7ddd4c10b651cb4de313a51e4dca
amcypher <13513389+amcypher@users.noreply.github.com>
2024-05-16 08:05:27 -0400

    Update scrubba.py

--- scrubba.py
+++ scrubba.py
@@ -1,54 +1,56 @@
-#Necessary Imports
-from unittest import result
+#headers = {"Content-Type": "application/json", "X-Auth-Key": key, "X-Auth-Email": email}
+# Necessary Imports
 import requests
-import os
 import json
 from pprint import pprint
 
-
+# URL to fetch the data
 url = "https://raw.githubusercontent.com/amcypher/PII-Scrubber-and-Sample-Data/main/pii-json.json"
-#headers = {"Content-Type": "application/json", "X-Auth-Key": key, "X-Auth-Email": email}
+# If you need to auth
+# headers = {"Content-Type": "application/json", "X-Auth-Key": key, "X-Auth-Email": email}
 
-#Method to pull data from the API      
+# Method to pull data from the API
 def pull_data(url):
-
-    #Set our headers for the HTTP Request
+    # Set our headers for the HTTP Request
     headers = {"Content-Type": "application/json"}
-    #Try catch to handle failed HTTP Requests
+    # Try catch to handle failed HTTP Requests
     try:
         response = requests.get(url, headers=headers)
-        #Return the response if HTTP 200
-        if response.status_code == 200:return json.loads(response.text)
-        #Non 200 Response
+        # Return the response if HTTP 200
+        if response.status_code == 200:
+            return response.json()
+        # Non 200 Response
         else:
-            print('Status Code' + response.status_code)
+            print(f'Status Code: {response.status_code}')
             return False
-    except:
-        #Exception Occured
-        print("API Fetch Failed")
+    except Exception as e:
+        # Exception occurred
+        print(f"API Fetch Failed: {e}")
         return False
 
-def scrubba(result):
-    #Removing the PII that we know exists in these Key Value Pairs
-    #Could wrap in a try catch to be wary of KeyError Exceptions etc but we'll assume the data always has these fields
-    #Object references passed to this function
-    result['pii_instances'][2]['ssn'] = "xxx-xx-xxxx"  
-    result['pii_instances'][1]['date_of_birth'] = 'xxxx/xx/xx'
+def scrubba(data):
+    # Removing the PII that we know exists in these Key Value Pairs
+    # Object references passed to this function
+    try:
+        data['pii_instances']['ssn'] = "xxx-xx-xxxx"
+        data['pii_instances']['date_of_birth'] = 'xxxx/xx/xx'
+    except KeyError as e:
+        print(f"KeyError: {e}")
 
-#Main
+# Main
 def main():
-    #Get the data
+    # Get the data
     my_results = pull_data(url)
-    #Check the outcome
-    if my_results != False:
-        #Scrub the data
+    # Check the outcome
+    if my_results:
+        # Scrub the data
         scrubba(my_results)
-        #Print the keys
+        # Print the keys
         pprint(my_results.keys())
-        #Print the scrubbed version
+        # Print the scrubbed version
         pprint(my_results)
     else:
-        #Something went wrong
+        # Something went wrong
         print("Exiting Program")
         exit()
 
